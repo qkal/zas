66890bd4dfb1be78be757b3bb966ba52ef9a186c
Qkal <Kal@lovablia.com>
2026-05-03 01:15:20 +0200

    feat(layout): inject custom TitleBar above all routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import '../app.css'
+  import TitleBar from '$lib/components/bliss/TitleBar.svelte'
   import type { Snippet } from 'svelte'
 
   type Props = { children: Snippet }
   let { children }: Props = $props()
 </script>
 
-{@render children()}
+<div class="flex h-screen flex-col">
+  <TitleBar />
+  <div class="flex-1 overflow-hidden">
+    {@render children()}
+  </div>
+</div>
